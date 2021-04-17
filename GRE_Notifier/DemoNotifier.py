373c21d74e9a526d9029fadb1753ce2d2edcc493
Rudviq <60641746+Rudviq@users.noreply.github.com>
2021-04-17 14:55:08 +0530

    Add files via upload

---
+++ GRE_Notifier/DemoNotifier.py
@@ -0,0 +1,34 @@
+#Code written by:Rudviq Bhavsar
+#Final testing done on:15/12/2020
+
+#Notifier to display Common GRE words .
+import random
+from plyer import notification
+import openpyxl as op
+import time
+
+
+def notifyMe(title,message):
+    notification.notify(
+        title=title,
+        message= message,
+        app_icon="C:\\Users\\Rudviq\\PycharmProjects\\Notify\\Common.ico",
+        timeout=4,
+    )
+
+if __name__=='__main__':
+    # while True:
+    #     notifyMe("Hey Rud, This is your first notifier","Reminding you to upgrade this App for Vocab reminder!.")
+    #     time.sleep(60*60)
+    while True:
+        wb=op.load_workbook("GREWords.xlsx")
+        # print(wb.sheetnames)
+        # print(wb.active.title)
+        sh1=wb['Common Words']
+        # print(sh1['B2'].value)
+        n=random.randrange(2,307)
+        b='B'+ str(n)
+        c='C'+str(n)
+        notifyMe(sh1[b].value,sh1[c].value)
+        time.sleep(10*60)
+
